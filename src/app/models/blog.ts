640af832db2d8a75ae9e43c21d63f0c9b0935cba
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-05-05 13:43:21 +0500

    adding model and connecting mongoDB database

---
+++ src/app/models/blog.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const SubheadingSchema = new mongoose.Schema({
+    image:{
+        type: String,
+        // required: true
+    },
+    title: {
+        type: String,
+        // required: true
+    },
+    content: {
+        type: String,
+        // required: true
+    }
+});
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    maincontent: {
+        type: String,
+        required: true
+    },
+    categorie: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    subContent: [SubheadingSchema] 
+});
+const BlogModel = mongoose.models.Blog || mongoose.model('Blog', BlogSchema);
+export default BlogModel;
---
+++ src/database/mongodb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connectDb = async () => {
+    if (isConnected) {
+        console.log("Using existing database connection");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Database connected successfully");
+        isConnected = true;
+    } catch (error) {
+        console.log("Error while connecting with the database", error);
+    }
+}
+
+export default connectDb;
